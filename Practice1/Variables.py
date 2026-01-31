1dcb3978f07cf7f591fee615b25444d071f57204
Adilkhan <adilkhankumar8@gmail.com>
2026-01-31 15:50:53 +0500

    seventh commit

---
+++ Practice1/Variables.py
@@ -0,0 +1,29 @@
+num = 23
+name = "Adilkhan"
+print(num)
+print(name)
+
+num = 23        # num is of type int
+name = "Max"    # name is now of type str
+print(num)
+print(name)
+
+num_str = str(10)   # num_str will be '10'
+num_int = int(10)   # num_int will be 10
+num_float = float(10)  # num_float will be 10.0
+print(num_str)
+print(num_int)
+print(num_float)
+
+num = 10
+name = "Adilkhan"
+print(type(num))
+print(type(name))
+
+name = "Adilkhan"
+# is the same as
+name = 'Adilkhan'
+
+a = 23
+A = "Adilkhan"
+# A will not overwrite a
