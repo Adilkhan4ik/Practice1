a00eaf330e87ac1fe3bedd943950250a3734de87
Adilkhan <adilkhankumar8@gmail.com>
2026-01-31 15:46:37 +0500

    fifth commit

---
+++ Practice1/String.py
@@ -0,0 +1,21 @@
+print("Life is amazing")
+print("He is called 'Bob'")
+print('He is called "Bob"')
+#--------------------------------
+b = """Programming in Python is flexible,
+fun to learn,
+popular in the tech world,
+and perfect for newcomers."""
+print(b)
+#--------------------------------------
+b = "Python rocks!"
+print(b[2])
+#------------------------------------
+for y in "banana":
+    print(y)
+#----------------------------------
+b = "Python rocks!"
+print(len(b))
+#-------------------------------------
+message = "Acquiring new knowledge is extremely beneficial!"
+print("beneficial" in message)
