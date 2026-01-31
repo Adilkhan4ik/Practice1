f344a75b7b02ae61120bdcd37da55e5ec9b86d5b
Adilkhan <adilkhankumar8@gmail.com>
2026-01-31 15:52:10 +0500

    Eighth commit

---
+++ Practice1/Data_types.py
@@ -0,0 +1,21 @@
+num = 55
+pi_val = 3.1415
+name = "Alex"
+is_active = True
+comp = complex(3j)
+fruits = list(("apple", "banana", "kiwi"))
+colors = tuple(("yellow", "purple", "pink"))
+numbers = range(5)
+info = dict(country="Kazakhstan", population=20_000_000)
+animals = set(("lion", "tiger", "elephant"))
+
+print(type(num))       # <class 'int'>
+print(type(pi_val))    # <class 'float'>
+print(type(name))      # <class 'str'>
+print(type(is_active)) # <class 'bool'>
+print(type(comp))      # <class 'complex'>
+print(type(fruits))    # <class 'list'>
+print(type(colors))    # <class 'tuple'>
+print(type(numbers))   # <class 'range'>
+print(type(info))      # <class 'dict'>
+print(type(animals))   # <class 'set'>
