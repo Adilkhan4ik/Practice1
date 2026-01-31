c2ca25f34a14ab09fae6e614b93a6b370d5abbd4
Adilkhan <adilkhankumar8@gmail.com>
2026-01-31 15:42:03 +0500

    third commit

---
+++ Practice1/Syntax.py
@@ -0,0 +1,19 @@
+if 6 > 2:
+  print("Six is greater than two!")
+
+
+if 7 > 2:
+ print("Seven is greater than two!") 
+
+
+if 7 > 2:
+    print("Seven is greater than two!") 
+    
+x = 99
+y = "Hello, Almaty"
+print(x)
+print(y)
+#This is a comment.
+print("Hello, Atyrau!")
+
+print("Hello"); print("How are you"); print("ok bye!")
