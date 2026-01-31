38e4486025475edcc9975286bf8e653c24653796
Adilkhan <adilkhankumar8@gmail.com>
2026-01-31 15:40:09 +0500

    second commit

---
+++ Practice1/Getting started.py
@@ -0,0 +1,16 @@
+import sys
+# Import the sys module — it provides system-specific parameters and functions
+
+print(sys.version)
+# Print the current Python version installed on the computer
+
+if 12 > 10:
+  print("Twelve is greater than ten!")
+
+exit()
+# Stop the program immediately
+# Any code written after this line will NOT run
+
+print(30 + 30)
+status = "Python is installed and running correctly"
+print(status)
