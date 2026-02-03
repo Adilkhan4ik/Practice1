6a5d7c6527511def704d1181157b77902db682e2
Adilkhan <adilkhankumar8@gmail.com>
2026-02-03 16:47:35 +0500

    1nddвsdd commit

---
+++ Practice2/Iteration over collection.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3]
+
+# 1: For loop over list
+for n in numbers:
+    print(n)
+
+# 2: For loop over set
+s = {10, 20, 30}
+for n in s:
+    print(n)
+
+# 3: For loop over dictionary keys
+d = {"a": 1, "b": 2}
+for key in d:
+    print(key)
+
+# 4: For loop over dictionary values
+for value in d.values():
+    print(value)
+
+# 5: For loop over dictionary items
+for key, value in d.items():
+    print(key, value)
