6e62f1678503201df685c4c71427153906299ec1
Adilkhan <adilkhankumar8@gmail.com>
2026-02-03 16:46:01 +0500

    1nddвdd commit

---
+++ Practice2/Dictionaries.py
@@ -0,0 +1,18 @@
+# 1: Create dict
+d = {"name": "Ali", "age": 17}
+print(d)
+
+# 2: Access value
+print(d["name"])
+
+# 3: Add item
+d["city"] = "Almaty"
+print(d)
+
+# 4: Keys and Values
+print(d.keys())
+print(d.values())
+
+# 5: Update value
+d["age"] = 18
+print(d)
