6e8fa3692c0024208070247f2e459728c572478d
Adilkhan <adilkhankumar8@gmail.com>
2026-02-03 16:41:30 +0500

    1nddd commit

---
+++ Practice2/Tuples.py
@@ -0,0 +1,16 @@
+# 1: Create a tuple
+tuple = (1, 2, 3)
+print(tuple)
+
+# 2: Indexing
+print(tuple[0])
+
+# 3: Slicing
+print(tuple[1:])
+
+# 4: Length
+print(len(tuple))
+
+# 5: Tuple unpacking
+a, b, c = tuple
+print(a, b, c)
