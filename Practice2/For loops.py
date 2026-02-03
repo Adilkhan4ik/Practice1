fb41f14034b5bbd8ece2fc4923884536ecb6c33c
Adilkhan <adilkhankumar8@gmail.com>
2026-02-03 16:38:56 +0500

    2ndd commit

---
+++ Practice2/For loops.py
@@ -0,0 +1,23 @@
+# 1: Print numbers 1 to 5
+for i in range(1, 6):
+    print(i)
+
+# 2: Sum of numbers 1 to 5
+total = 0
+for i in range(1, 6):
+    total += i
+print(total)
+
+# 3: Print squares of 1 to 5
+for i in range(1, 6):
+    print(i**2)
+
+# 4: Iterate over a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# 5: Nested for loop
+for i in range(1, 4):
+    for j in range(1, 3):
+        print(i, j)
